f0e2da6b702ea2ae128dbed2de209874c23e0019
M Afzaal Afzal <afzaal1721@gmail.com>
2021-08-17 16:27:26 +0500

    Logo button bug fixed

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -54,7 +54,7 @@ const Header = () => {
 
                     {/* left section of header*/}
                     <HStack spacing={8}>
-                        <Heading fontSize={['30px', '32px', '36px']}>
+                        <Heading as={'button'} cursor={'pointer'} fontSize={['30px', '32px', '36px']}>
                             Learning<Box display={'inline-block'} color={'brand.blue'}>Path</Box>
                         </Heading>
 
